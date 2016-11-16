f20dbf476f4e743acca0a9f54a68ad6440750bd6
Nicolo Crisafulli <nicolo.crisafulli@villaggio.org>
2016-11-16 15:29:18 +0100

    Aggiunte function

--- Classes/School.php
+++ Classes/School.php
@@ -1,8 +1,20 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace Classes;
 
+class School{
+    private $name;
+    private $student;
+    
+    public function __construct($name) {
+        $this->name = $name;
+    }
+    
+    public function addStudent(Student $stud){
+        
+    }
+    
+    public function getStudents(){
+        
+    }
+}
--- Classes/Teacher.php
+++ Classes/Teacher.php
@@ -1,2 +1,39 @@
 <?php
 
+namespace Classes;
+
+class Teacher{
+
+    private $name;
+    private $age;
+    private $course;
+
+    public function __construct($name,$age,$email,$course=null) {
+        $this->name=$name;
+        $this->age=$age;
+        $this->email=$email;
+        $this->course=$course;
+    }
+
+    public function __toString() {
+        return "Nome: $this->nome Età: $this->age Email: $this->email Corso: $this->course";
+    }
+
+    public function getCourse(){
+
+    }
+
+    public function setCourse(Course $course){
+
+    }
+
+    public function getSchool(){
+
+    }
+
+    public function setSchool(School $stud){
+        
+    }
+
+
+}
